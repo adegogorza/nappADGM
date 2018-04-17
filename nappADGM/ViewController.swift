a462e9687c6c01b56aab35ce8fa67fac8fe78c22
Alexander Moravcsik <adegogorza@2a01cb040069180000c1dd6b95b28167.ipv6.abo.wanadoo.fr>
2018-04-17 19:19:14 +0200

    Initial Commit

---
+++ nappADGM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nappADGM
+//
+//  Created by Alexander de Gogorza Moravcsik on 17/04/2018.
+//  Copyright © 2018 ADGM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
